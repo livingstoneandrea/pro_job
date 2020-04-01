53ad23765cb69acf4e1d6af842f820578d0c4d44
livin <livin@kali>
2020-04-01 09:11:55 +0300

    configured mailbackend

--- Job_pro/settings.py
+++ Job_pro/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'youremail@gmail.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_HOST_USER = 'livingstoneandrea1998@gmail.com'
+EMAIL_HOST_PASSWORD = '0799998812'
 EMAIL_PORT = 587
 
 
